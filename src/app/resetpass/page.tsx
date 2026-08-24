799a2931016a083a15124143b1d4f0699a2dfdba
anze10 <anze.repse@gmail.com>
2026-08-24 09:33:35 +0200

    Fix reset password page - remove incorrect session requirement

--- src/app/resetpass/page.tsx
+++ src/app/resetpass/page.tsx
@@ -1,12 +1,6 @@
 import ResetPasswordPage from "~/app/resetpass/components/Resetpasswordpage";
-import { getCurrentSession } from "~/server/LOGIN_LUCIA_ACTION/session";
-import { redirect } from "next/dist/client/components/navigation";
 
-export default async function Home() {
-    const { user } = await getCurrentSession();
-    if (user === null) {
-        return redirect("/");
-    }
+export default function Home() {
     return (
         <>
             <ResetPasswordPage />
