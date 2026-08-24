2f67a719f9fea8ef915d2edd66c6133dbd9ebfbe
anze10 <anze.repse@gmail.com>
2026-08-24 09:41:46 +0200

    Fix build error - wrap useSearchParams in Suspense boundary

--- src/app/resetpass/page.tsx
+++ src/app/resetpass/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import ResetPasswordPage from "~/app/resetpass/components/Resetpasswordpage";
 
 export default function Home() {
     return (
-        <>
+        <Suspense fallback={<div>Nalagam...</div>}>
             <ResetPasswordPage />
-        </>
+        </Suspense>
     );
 }
